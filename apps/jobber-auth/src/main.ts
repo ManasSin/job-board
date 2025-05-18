ff990a809d4b46ef7134366c74c8f521b20823ae
ManasSin <maltisadan12@gmail.com>
2025-05-19 00:16:46 +0530

    feat: Refactor NestJS application initialization in one function InitNestApp
    - Introduced InitNestApp function to centralize application setup logic.
    - Removed redundant code from main.ts files in jobber-auth and jobs.
    - Updated index.ts to export the new InitNestApp function.

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -3,22 +3,16 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ConfigService } from '@nestjs/config';
-import * as cookieParser from 'cookie-parser';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
 import { join } from 'path';
+import { InitNestApp } from '@jobber-fresh/nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.setGlobalPrefix(globalPrefix);
-  app.use(cookieParser());
-  const port = app.get(ConfigService).getOrThrow('PORT');
+  await InitNestApp(app);
   app.connectMicroservice<GrpcOptions>({
     transport: Transport.GRPC,
     options: {
@@ -27,10 +21,6 @@ async function bootstrap() {
     },
   });
   await app.startAllMicroservices();
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
 }
 
 bootstrap();
--- apps/jobs/src/main.ts
+++ apps/jobs/src/main.ts
@@ -3,24 +3,13 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
-
 import { AppModule } from './app/app.module';
-import { ConfigService } from '@nestjs/config';
+import { InitNestApp } from '@jobber-fresh/nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.setGlobalPrefix(globalPrefix);
-  app.use(cookieParser());
-  const port = app.get(ConfigService).getOrThrow('PORT');
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  await InitNestApp(app);
 }
 
 bootstrap();
